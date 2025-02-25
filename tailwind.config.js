df1209755af5b494542745bc79c61eaf8455d06a
Bilal <bilal.kanjelkheir@gmail.com>
2025-02-25 18:44:08 +0200

    fix dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media', // or 'class'
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
+        'gradient-to-l': 'linear-gradient(to left, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'gradient-start': '#ff7e5f',
+        'gradient-end': '#feb47b',
+        'dark-bg': '#1a202c', // Define your dark background color here
+      },
+    },
   },
   plugins: [],
 }
