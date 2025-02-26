7871156cafbc8f849436503e6fad8dcac7ef1903
Bilal <bilal.kanjelkheir@gmail.com>
2025-02-26 13:25:14 +0200

    Add profile component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { useNavigate } from 'react-router-dom';
 import './index.css';
 import { useGetUserQuery } from './App/Apis/GithubApi.js';
 import {ReactComponent as SearchIcon} from './assets/magnifying-glass.svg';
+import Profile from './components/profile.jsx';
 
 const App = () => {
   const [input, setInput] = useState("");
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
+  const { data, isError, isLoading} = useGetUserQuery('bilal-algorithms');
+  if (localStorage.getItem("username") === null) {
+    localStorage.setItem("username", "bilal-algorithms");
+  }
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-dark-bg">
       <div className="w-full ml-[85%] mt-4">
-        <p className="text-black dark:text-white font-bold text-lg">Developer: <a target="_blank" rel="noreferrer" className="border-b-2 border-b-black hover:text- hover:border-b-4 duration-200 hover:border-b-blue-500" href="https://github.com/bilal-algorithms">Bilal Kanjelkheir</a></p>
+        <p className="text-black dark:text-white font-bold text-lg">Developer: <a target="_blank" rel="noreferrer" className="border-b-2 dark:border-b-blue-500 border-b-black hover:text- hover:border-b-4 duration-200 hover:border-b-blue-500" href="https://github.com/bilal-algorithms">Bilal Kanjelkheir</a></p>
       </div>
       <div className="flex flex-col mt-28 w-full px-48 h-full flex-wrap">
         <div className="w-[90%] mx-auto flex items-center justify-center mb-40">
-          <p className="font-bold text-6xl bg-gradient-to-r from-gradient-start to-gradient-end bg-clip-text text-transparent">
-            Find More About Your Developer on Github...
+          <p className="font-bold text-6xl dark:text-white">
+            Find More About Your <span className="font-bold text-6xl bg-gradient-to-r from-gradient-start to-gradient-end bg-clip-text text-transparent">Developer</span> on Github...
           </p>
         </div>
       </div>
@@ -26,14 +32,18 @@ const App = () => {
         placeholder="Search for a Developer..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        className="w-full h-full outline-none border-none pl-2 rounded-xl"
+        className="w-full h-full outline-none border-none pl-2 rounded-xl dark:bg-transparent"
         />
-        <button className="px-4 hover:bg-slate-300 rounded-md duration-200">
+        <button onClick={() => setUsername(input)} className="px-4 hover:bg-slate-300 rounded-md duration-200 dark:hover:bg-slate-700">
           <SearchIcon className="w-5 h-5" />
         </button>
       </div>
+        <div className="relative top-10 mx-4 flex flex-col-reverse">
+        <button onClick={() => {localStorage.setItem("username", input); console.log(username)}} className="text-blue-400 mt-2 hover:underline cursor-pointer w-fit">Save search</button>
+          <Profile username={username ? username : localStorage.getItem("username")} />
+        </div>
+        
     </div>
   )
 }
-
 export default App;
