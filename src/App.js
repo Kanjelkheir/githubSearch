bd05b4fd7292208a1be787c3e401f9f997552f06
Bilal <bilal.kanjelkheir@gmail.com>
2025-02-27 12:31:23 +0200

    Change container and text sizes for smaller devices

--- src/App.js
+++ src/App.js
@@ -30,40 +30,38 @@ const App = () => {
     }
   }
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-dark-bg">
-      <div className="w-full ml-[85%] mt-4">
+    <div className="flex flex-col min-h-screen bg-white dark:bg-dark-bg w-full">
+      <div className="w-full ml-auto mt-4">
         <p className="text-black dark:text-white font-bold text-lg">Developer: <a target="_blank" rel="noreferrer" className="border-b-2 dark:border-b-blue-500 border-b-black hover:text- hover:border-b-4 duration-200 hover:border-b-blue-500" href="https://github.com/bilal-algorithms">Bilal Kanjelkheir</a></p>
       </div>
-      <div className="flex flex-col mt-28 w-full px-48 h-full flex-wrap">
-        <div className="w-[90%] mx-auto flex items-center justify-center mb-40">
-          <p className="font-bold text-6xl dark:text-white">
-            Find More About Your <span className="font-bold text-6xl bg-gradient-to-r from-gradient-start to-gradient-end bg-clip-text text-transparent">Developer</span> on Github...
+      <div className="flex flex-col mt-28 w-full px-4 md:px-48 h-full flex-wrap">
+        <div className="w-full mx-auto flex items-center justify-center mb-40">
+          <p className="font-bold text-4xl md:text-6xl dark:text-white">
+            Find More About Your <span className="font-bold text-4xl md:text-6xl bg-gradient-to-r from-gradient-start to-gradient-end bg-clip-text text-transparent">Developer</span> on Github...
           </p>
         </div>
       </div>
-      <div className="flex flex-row shadow-md shadow-slate-300 h-12 w-[25%] mx-auto rounded-xl">
+      <div className="flex flex-row shadow-md shadow-slate-300 h-12 w-full md:w-[25%] mx-auto rounded-xl">
         <input 
-        type="text"
-        placeholder="Search for a Developer..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        // add event listener for enter key
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            localStorage.setItem("username", input);
-            setUsername(input);
-          }
-        }}
-        className="w-full h-full outline-none border-none pl-2 rounded-xl dark:bg-transparent dark:text-blue-400 text-blue-400"
+          type="text"
+          placeholder="Search for a Developer..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              localStorage.setItem("username", input);
+              setUsername(input);
+            }
+          }}
+          className="w-full h-full outline-none border-none pl-2 rounded-xl dark:bg-transparent dark:text-blue-400 text-blue-400"
         />
         <button onClick={() => setUsername(input)} className="px-4 hover:bg-slate-300 rounded-md duration-200 dark:hover:bg-slate-700">
           <SearchIcon className="w-5 h-5" />
         </button>
       </div>
-        <div className="relative top-10 mx-4 flex flex-col-reverse">
-          <Profile username={username ? username : localStorage.getItem("username")} onDelete={handleDelete} deleted={deleted} input={username} />
-        </div>
-      
+      <div className="relative top-10 mx-4 flex flex-col-reverse">
+        <Profile username={username ? username : localStorage.getItem("username")} onDelete={handleDelete} deleted={deleted} input={username} />
+      </div>
     </div>
   )
 }
