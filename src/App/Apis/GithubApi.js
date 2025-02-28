9d5be1bfe05fbaa30e98022b55c5efdf71a9933f
Bilal <bilal.kanjelkheir@gmail.com>
2025-02-28 12:38:35 +0200

    Extend api calls to 5000 per hour

--- src/App/Apis/GithubApi.js
+++ src/App/Apis/GithubApi.js
@@ -2,7 +2,15 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const GithubApi = createApi({
     reducerPath: "Getuser",
-    baseQuery: fetchBaseQuery({baseUrl: "https://api.github.com"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://api.github.com",
+        prepareHeaders: (headers, {getState}) => {
+            const token = process.env.REACT_APP_GITHUB_TOKEN;
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+            return headers;
+        }
+    }),
     endpoints: (builder) => ({
         getUser: builder.query({query: (user) => `users/${user}`}),
     })
